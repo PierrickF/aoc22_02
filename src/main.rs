c40a5aab5b5df4eae24ebd7f961b3ecda336c5bb
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-12-05 10:18:04 +0100

    Print task 1 right answer

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ fn main() -> Result<(), Error> {
 }
 
 fn parse_file<R: Read>(input: R) {
+    let mut final_score: u32 = 0;
     let br = BufReader::new(input);
     for line in br.lines() {
-        // returns String
         let str_line = line.expect("Failed to read line");
         let vec = string_to_vector(str_line);
-        println!("{:?}", vec);
+        final_score = score_round(vec) + final_score;
+        println!("\n");
     }
+    println!("{final_score}");
 }
 
 fn string_to_vector(line: String) -> Vec<char> {
@@ -23,4 +25,46 @@ fn string_to_vector(line: String) -> Vec<char> {
         vec.push(iter.next().unwrap());
     }
     vec
+}
+
+fn score_round(vec: Vec<char>) -> u32 {
+    let mut score: u32 = 0;
+
+    match vec[2] {
+        'X' => {
+            score += 1;
+            println!("1 point for playing rock");
+        }
+        'Y' => {
+            score += 2;
+            println!("2 points for playing paper");
+        }
+        'Z' => {
+            score += 3;
+            println!("3 points for playing scissors");
+        }
+        _ => panic!("uh oh"),
+    }
+
+    if vec[2] == 'X' && vec[0] == 'A' {
+        score += 3;
+        println!("3 points for draw");
+    } else if vec[2] == 'Y' && vec[0] == 'B' {
+        score += 3;
+        println!("3 points for draw");
+    } else if vec[2] == 'Z' && vec[0] == 'C' {
+        score += 3;
+        println!("3 points for draw");
+    } else if vec[2] == 'X' && vec[0] == 'C' {
+        score += 6;
+        println!("6 points for win");
+    } else if vec[2] == 'Y' && vec[0] == 'A' {
+        score += 6;
+        println!("6 points for win");
+    } else if vec[2] == 'Z' && vec[0] == 'B' {
+        score += 6;
+        println!("6 points for win");
+    }
+
+    score
 }
